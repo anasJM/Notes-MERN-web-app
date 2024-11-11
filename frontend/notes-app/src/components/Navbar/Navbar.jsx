fa4f1e95eec3e652c0a51bb0f1414386d0a13a3a
anasJM <anasjamlaoui@gmail.com>
2024-11-11 19:34:26 +0100

    login form

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='bg-white flex item-center justify-between px-5 py-3 drop-shadow'>
-        <h2 className='text-xl font-medium text-black py-2'>Notes</h2>
+    <div className="bg-white flex items-center justify-between px-5 py-3 drop-shadow">
+      <h2 className="text-xl font-medium text-black py-2">Notes</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,20 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
+import PasswordInput from "../../components/Input/PasswordInput";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <Navbar />
-      <div>
-        <div>
-          <form onSubmit={() => {}}>
-            <h4 className='text-2xl mb-7'>Login</h4>
-            <input type="text" placeholder='Email' className='input-box' />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleLogin}>
+            <h4 className="text-2xl mb-7">Login</h4>
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Not registred yet?{" "}
+              <Link
+                to={"/signup"}
+                className="font-medium text-primary underline"
+              >
+                Create an Account
+              </Link>
+            </p>
           </form>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
