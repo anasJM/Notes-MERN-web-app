d17cb16411cfcf2e1725c01145dc8948bce0ef73
anasJM <anasjamlaoui@gmail.com>
2024-11-11 20:42:51 +0100

    sign up form UI

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
 import PasswordInput from "../../components/Input/PasswordInput";
+import { validateEmail } from "../../utils/helper";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -10,6 +11,20 @@ const Login = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("please enter the password!");
+      return;
+    }
+
+    setError("");
+
+    // login API call
   };
 
   return (
@@ -31,6 +46,9 @@ const Login = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
+
+            {/* error message */}
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
 
             <button type="submit" className="btn-primary">
               Login
--- frontend/notes-app/src/pages/Signup/Signup.jsx
+++ frontend/notes-app/src/pages/Signup/Signup.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import PasswordInput from "../../components/Input/PasswordInput";
+import { validateEmail } from "../../utils/helper";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-export default Signup
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setError("please enter your name!");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("please enter the password!");
+      return;
+    }
+
+    setError("");
+
+    // signup API call
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl mb-7">Sign up</h4>
+
+            <input
+              type="text"
+              placeholder="Name"
+              className="input-box"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {/* error message */}
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
+            <button type="submit" className="btn-primary">
+              Create Account
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Already have account?{" "}
+              <Link
+                to={"/login"}
+                className="font-medium text-primary underline"
+              >
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
