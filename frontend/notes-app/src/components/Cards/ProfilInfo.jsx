98093e47a239aaf805938a9d8092bbf47d17a272
anasJM <anasjamlaoui@gmail.com>
2024-11-12 15:55:14 +0100

    search bar component

--- frontend/notes-app/src/components/Cards/ProfilInfo.jsx
+++ frontend/notes-app/src/components/Cards/ProfilInfo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { getInitials } from "../../utils/helper";
 
-const ProfilInfo = () => {
+const ProfilInfo = ({ onLogout }) => {
   return (
     <div className="flex items-center gap-3">
       <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
@@ -9,8 +9,10 @@ const ProfilInfo = () => {
       </div>
 
       <div>
-        <p className="text-sm font-medium">Anas</p>
-        <button className="text-sm text-slate-700 underline">Logout</button>
+        <p className="text-sm font-medium">Anas Jamlaoui</p>
+        <button className="text-sm text-slate-700 underline" onClick={onLogout}>
+          Logout
+        </button>
       </div>
     </div>
   );
