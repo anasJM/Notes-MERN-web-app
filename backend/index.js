0ecf93f38c6d777302975bf6f59a87c019f58c16
anasJM <anasjamlaoui@gmail.com>
2024-11-16 23:16:51 +0100

    edit note API

--- backend/index.js
+++ backend/index.js
@@ -136,12 +136,6 @@ app.post("/add-note", authenticateToken, async (req, res) => {
     return res.status(400).json({ error: true, message: "Enter the content" });
   }
 
-  // if (!user) {
-  //   return res
-  //     .status(400)
-  //     .json({ error: true, message: "User not authenticated" });
-  // }
-
   try {
     const note = new Note({
       title,
@@ -155,6 +149,44 @@ app.post("/add-note", authenticateToken, async (req, res) => {
     return res.json({
       error: false,
       message: "Note added successfully!",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: true,
+      message: err.message,
+    });
+  }
+});
+
+// ********** Edit note *********** //
+app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
+  const noteId = req.params.noteId;
+  const { title, content, tags, isPinned } = req.body;
+  const { user } = req.user;
+
+  if (!title && !content && !tags) {
+    return res
+      .status(400)
+      .json({ error: true, message: "No changes provided" });
+  }
+
+  try {
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+    if (!note) {
+      return res.status(404).json({ error: true, message: "Note not found" });
+    }
+
+    if (title) note.title = title;
+    if (content) note.content = content;
+    if (tags) note.tags = tags;
+    if (isPinned) note.isPinned = isPinned;
+
+    await note.save();
+
+    return res.json({
+      error: false,
+      message: "Note has been updated successfully",
     });
   } catch (err) {
     return res.status(500).json({
